4d2920ff8668508dc4212dff7226d79cdc3a386a
KiritoDev <alexis.contreras@r9studio.com>
2019-09-14 16:22:30 -0500

    Handle Success

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("bodyParser");
+
+app.use(bodyParser.json());
 
 app.post("/gl", (req, res)=> {
     console.log(req)
